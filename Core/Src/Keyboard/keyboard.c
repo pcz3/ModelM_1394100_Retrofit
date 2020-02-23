6fb362c7523e59416d8b3235e72cd972432b56d3
paul.czeresko <paul@czeresko.com>
2020-02-22 22:48:44 -0500

    WIP: begin reassigning key structs
    Cleaned up struct initializations. Need to examine circuit/schematic
    to assign real values.

--- Core/Src/Keyboard/keyboard.c
+++ Core/Src/Keyboard/keyboard.c
@@ -217,436 +217,9 @@ void keyboardInit()
 			.lastTrigger = 0,
 			.reportIndex = 0
 	};
-	static key_struct_t kr0c1 = {
-			.name = "W",
-			.val = { KEY_W, KEY_2 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c2 = {
-			.name = "E",
-			.val = { KEY_E, KEY_3 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c3 = {
-			.name = "R",
-			.val = { KEY_R, KEY_4 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c4 = {
-			.name = "T",
-			.val = { KEY_T, KEY_5 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c5 = {
-			.name = "Y",
-			.val = { KEY_Y, KEY_6 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c6 = {
-			.name = "U",
-			.val = { KEY_U, KEY_7 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c7 = {
-			.name = "I",
-			.val = { KEY_I, KEY_8 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c8 = {
-			.name = "O",
-			.val = { KEY_O, KEY_9 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c9 = {
-			.name = "P",
-			.val = { KEY_P, KEY_0 },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr0c10 = {
-			.name = "BSPACE",
-			.val = { KEY_BACKSPACE, KEY_DELETE },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	/* Row 01 ----------------------------------------------------------------*/
-	static key_struct_t kr1c0 = {
-			.name = "A",
-			.val = { KEY_A, KEY_A },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c1 = {
-			.name = "S",
-			.val = { KEY_S, KEY_S },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c2 = {
-			.name = "D",
-			.val = { KEY_D, KEY_D },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c3 = {
-			.name = "F",
-			.val = { KEY_F, KEY_F },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c4 = {
-			.name = "G",
-			.val = { KEY_G, KEY_G },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c5 = {
-			.name = "H",
-			.val = { KEY_H, KEY_H },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c6 = {
-			.name = "J",
-			.val = { KEY_J, KEY_J },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c7 = {
-			.name = "K",
-			.val = { KEY_K, KEY_K },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c8 = {
-			.name = "L",
-			.val = { KEY_L, KEY_L },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c9 = {
-			.name = "NA",
-			.val = { KEY_NONE, KEY_NONE },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr1c10 = {
-			.name = "ENTER",
-			.val = { KEY_ENTER, KEY_ENTER },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	/* Row 02 ----------------------------------------------------------------*/
-	static key_struct_t kr2c0 = {
-			.name = "Z",
-			.val = { KEY_Z, KEY_Z },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c1 = {
-			.name = "X",
-			.val = { KEY_X, KEY_X },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c2 = {
-			.name = "C",
-			.val = { KEY_C, KEY_C },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c3 = {
-			.name = "V",
-			.val = { KEY_V, KEY_V },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c4 = {
-			.name = "B",
-			.val = { KEY_B, KEY_B },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c5 = {
-			.name = "N",
-			.val = { KEY_N, KEY_N },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c6 = {
-			.name = "M",
-			.val = { KEY_M, KEY_M },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c7 = {
-			.name = "NA",
-			.val = { KEY_NONE, KEY_NONE },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c8 = {
-			.name = "COMMA",
-			.val = { KEY_COMMA, KEY_DOT },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c9 = {
-			.name = "RSHIFT",
-			.val = { KEY_MOD_RSHIFT, KEY_MOD_RSHIFT },
-			.isMod = 1,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr2c10 = {
-				.name = "TAB",
-				.val = { KEY_TAB, KEY_ESC },
-				.isMod = 0,
-				.currState = 0,
-				.tempState = 0,
-				.stateChanged = 0,
-				.lastTrigger = 0,
-				.reportIndex = 0
-	};
-	/* Row 03 ----------------------------------------------------------------*/
-	static key_struct_t kr3c0 = {
-			.name = "NA",
-			.val = { KEY_NONE, KEY_NONE },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c1 = {
-			.name = "LCTRL",
-			.val = { KEY_MOD_LCTRL, KEY_MOD_LCTRL },
-			.isMod = 1,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c2 = {
-			.name = "LMETA",
-			.val = { KEY_MOD_LMETA, KEY_MOD_LMETA },
-			.isMod = 1,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c3 = {
-			.name = "LALT",
-			.val = { KEY_MOD_LALT, KEY_MOD_LALT },
-			.isMod = 1,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c4 = {
-			.name = "FN",
-			.val = { 0xFF, 0xFF },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c5 = {
-			.name = "SPACE",
-			.val = { KEY_SPACE, KEY_SPACE },
-			.isMod = 0,
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c6 = {
-			.name = "NA",
-			.val = { KEY_NONE, KEY_NONE },
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c7 = {
-			.name = "LEFT",
-			.val = { KEY_LEFT, KEY_HOME },
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c8 = {
-			.name = "DOWN",
-			.val = { KEY_DOWN, KEY_DOWN },
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c9 = {
-			.name = "UP",
-			.val = { KEY_UP, KEY_UP },
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
-	static key_struct_t kr3c10 = {
-			.name = "RIGHT",
-			.val = { KEY_RIGHT, KEY_END },
-			.currState = 0,
-			.tempState = 0,
-			.stateChanged = 0,
-			.lastTrigger = 0,
-			.reportIndex = 0
-	};
+
 	/* TODO: make this const? */
-	static key_struct_t *theKeys[44] = {
+	static key_struct_t *theKeys[180] = {
 			&kr0c0, &kr0c1, &kr0c2, &kr0c3, &kr0c4, &kr0c5, &kr0c6, &kr0c7, &kr0c8, &kr0c9, &kr0c10,
 			&kr1c0, &kr1c1, &kr1c2, &kr1c3, &kr1c4, &kr1c5, &kr1c6, &kr1c7, &kr1c8, &kr1c9, &kr1c10,
 			&kr2c0, &kr2c1, &kr2c2, &kr2c3, &kr2c4, &kr2c5, &kr2c6, &kr2c7, &kr2c8, &kr2c9, &kr2c10,
@@ -670,6 +243,23 @@ void keyboardInit()
 			.port = ROW_3_GPIO_Port,
 			.pin = ROW_3_Pin
 	};
+	static gpio_struct_t row4 = {
+			.port = ROW_4_GPIO_Port,
+			.pin = ROW_4_Pin
+	};
+	static gpio_struct_t row5 = {
+			.port = ROW_5_GPIO_Port,
+			.pin = ROW_5_Pin
+	};
+	static gpio_struct_t row6 = {
+			.port = ROW_6_GPIO_Port,
+			.pin = ROW_6_Pin
+	};
+	static gpio_struct_t row7 = {
+			.port = ROW_7_GPIO_Port,
+			.pin = ROW_7_Pin
+	};
+
 	/* Initialize columns ----------------------------------------------------*/
 	static gpio_struct_t col0 = {
 			.port = COL_0_GPIO_Port,
@@ -715,17 +305,55 @@ void keyboardInit()
 			.port = COL_10_GPIO_Port,
 			.pin = COL_10_Pin
 	};
-	/* TODO: make this const? */
-	static gpio_struct_t *gpioRows[4] = { &row0, &row1, &row2, &row3 };
-	static gpio_struct_t *gpioCols[11] = {
-			&col0, &col1, &col2, &col3, &col4, &col5, &col6, &col7, &col8, &col9, &col10
+	static gpio_struct_t col11 = {
+			.port = COL_11_GPIO_Port,
+			.pin = COL_11_Pin
+	};
+	static gpio_struct_t col12 = {
+			.port = COL_12_GPIO_Port,
+			.pin = COL_12_Pin
+	};
+	static gpio_struct_t col13 = {
+			.port = COL_13_GPIO_Port,
+			.pin = COL_13_Pin
+	};
+	static gpio_struct_t col14 = {
+			.port = COL_14_GPIO_Port,
+			.pin = COL_14_Pin
+	};
+	static gpio_struct_t col15 = {
+			.port = COL_15_GPIO_Port,
+			.pin = COL_15_Pin
+	};
+	static gpio_struct_t col16 = {
+			.port = COL_16_GPIO_Port,
+			.pin = COL_16_Pin
+	};
+	static gpio_struct_t col17 = {
+			.port = COL_17_GPIO_Port,
+			.pin = COL_17_Pin
+	};
+	static gpio_struct_t col18 = {
+			.port = COL_18_GPIO_Port,
+			.pin = COL_18_Pin
+	};
+	static gpio_struct_t col19 = {
+			.port = COL_19_GPIO_Port,
+			.pin = COL_19_Pin
+	};
+
+	static gpio_struct_t *gpioRows[8] = {
+			&row0, &row1, &row2, &row3, &row4, &row5, &row6, &row7
+	};
+	static gpio_struct_t *gpioCols[20] = {
+			&col0, &col1, &col2, &col3, &col4, &col5, &col6, &col7, &col8, &col9,
+			&col10, &col11, &col12, &col13, &col14, &col15, &col16, &col17, &col18, &col19,
 	};
 
 	/* Initialize keyboard ---------------------------------------------------*/
 	keeb = (key_matrix_t) {
-		.numRows = 4,
-				.numCols = 11,
-				.rowPort = GPIOB,
+		.numRows = 8,
+				.numCols = 20,
 				.rowPins = gpioRows,
 				.colPins = gpioCols,
 				.keys = theKeys,
